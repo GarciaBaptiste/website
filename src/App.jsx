cc36b6ca617de35cb8cc03656b20254d18e4cdea
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-06-24 14:48:16 +0200

    feat: add info to table of content + set up prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
-import styled from 'styled-components'
-import GlobalFonts from './fonts/fonts'
+import "./App.css";
+import styled from "styled-components";
+import GlobalFonts from "./fonts/fonts";
 
 const Header = styled.header`
   position: fixed;
@@ -12,9 +12,9 @@ const Header = styled.header`
   background: black;
   color: var(--white);
   padding: 0 3rem;
-  font-family: 'JetBrains';
+  font-family: "JetBrains";
   font-size: var(--text-small);
-`
+`;
 
 const HeaderContent = styled.div`
   flex: 1;
@@ -30,13 +30,13 @@ const HeaderContent = styled.div`
   & > a:hover {
     color: lightblue;
   }
-`
+`;
 
-const HeaderContentLeft = styled(HeaderContent)``
+const HeaderContentLeft = styled(HeaderContent)``;
 
 const HeaderContentRight = styled(HeaderContent)`
-  justify-content: end
-`
+  justify-content: end;
+`;
 
 const FixedMosaic = styled.section`
   position: fixed;
@@ -45,7 +45,8 @@ const FixedMosaic = styled.section`
   top: 2rem;
   left: 0;
   pointer-events: none;
-`
+  z-index: 100;
+`;
 
 const ScrollMosaic = styled.section`
   position: fixed;
@@ -62,22 +63,22 @@ const ScrollMosaic = styled.section`
     display: none;
   }
 
-  @media (max-width: 3900px){
+  @media (max-width: 3900px) {
     grid-template-columns: repeat(6, 1fr);
   }
-  @media (max-width: 3250px){
+  @media (max-width: 3250px) {
     grid-template-columns: repeat(5, 1fr);
   }
-  @media (max-width: 2600px){
+  @media (max-width: 2600px) {
     grid-template-columns: repeat(4, 1fr);
   }
-  @media (max-width: 1950px){
+  @media (max-width: 1950px) {
     grid-template-columns: repeat(3, 1fr);
   }
-  @media (max-width: 1300px){
+  @media (max-width: 1300px) {
     grid-template-columns: repeat(2, 1fr);
   }
-  @media (max-width: 650px){
+  @media (max-width: 650px) {
     grid-template-columns: repeat(1, 1fr);
   }
 
@@ -100,60 +101,60 @@ const ScrollMosaic = styled.section`
     list-style: none;
     color: var(--grey1);
   }
-`
+`;
 
 const CardWrapper = styled.div`
   position: relative;
   border-radius: 2rem;
-  background: #FFF7EA;
+  background: #fff7ea;
   scroll-snap-align: start;
 
-  @media (max-height: 2800px){
+  @media (max-height: 2800px) {
     height: calc((100vh - 2rem) / 4);
   }
-  @media (max-height: 2100px){
+  @media (max-height: 2100px) {
     height: calc((100vh - 2rem) / 3);
   }
-  @media (max-height: 1400px){
+  @media (max-height: 1400px) {
     height: calc((100vh - 2rem) / 2);
   }
-  @media (max-height: 700px){
+  @media (max-height: 700px) {
     height: calc((100vh - 2rem) / 1);
   }
-`
+`;
 
 const CardBorder = styled.div`
   position: absolute;
   background: var(--white);
-`
+`;
 
 const BorderTop = styled(CardBorder)`
   top: -1px;
   left: 8rem;
   height: 1px;
   width: calc(100% - 16rem);
-`
+`;
 
 const BorderBottom = styled(CardBorder)`
   bottom: -1px;
   left: 8rem;
   height: 1px;
   width: calc(100% - 16rem);
-`
+`;
 
 const BorderLeft = styled(CardBorder)`
   top: 8rem;
   left: -1px;
   height: calc(100% - 16rem);
   width: 1px;
-`
+`;
 
 const BorderRight = styled(CardBorder)`
   top: 8rem;
   right: -1px;
   height: calc(100% - 16rem);
   width: 1px;
-`
+`;
 
 const Card = ({ children }) => {
   return (
@@ -164,8 +165,8 @@ const Card = ({ children }) => {
       <BorderRight />
       {children}
     </CardWrapper>
-  )
-}
+  );
+};
 
 const CardContainer = styled.div`
   display: flex;
@@ -173,7 +174,7 @@ const CardContainer = styled.div`
   margin: 3rem;
   width: calc(100% - 6rem);
   height: calc(100% - 6rem);
-`
+`;
 
 const PresentationCard = styled(CardWrapper)`
   background: lightblue;
@@ -181,58 +182,7 @@ const PresentationCard = styled(CardWrapper)`
   & > ${CardContainer} {
     flex-direction: column;
   }
-`
-
-const TOCCard = styled(CardWrapper)`
-  position: absolute;
-  background: black;
-  bottom: -1px;
-  right: -1px;
-  border-radius: 2rem 2rem 0 2rem;
-  
-  @media (max-width: 3900px){
-    width: calc(100% / 6 + 2px);
-  }
-  @media (max-width: 3250px){
-    width: calc(100% / 5 + 2px);
-  }
-  @media (max-width: 2600px){
-    width: calc(100% / 4 + 2px);
-  }
-  @media (max-width: 1950px){
-    width: calc(100% / 3 + 2px);
-  }
-  @media (max-width: 1300px){
-    width: calc(100% / 2 + 2px);
-  }
-  @media (max-width: 650px){
-    width: calc(100% / 1 + 2px);
-  }
-
-  @media (max-height: 2800px){
-    height: calc((100vh - 2rem) / 4 + 2px);
-  }
-  @media (max-height: 2100px){
-    height: calc((100vh - 2rem) / 3 + 2px);
-  }
-  @media (max-height: 1400px){
-    height: calc((100vh - 2rem) / 2 + 2px);
-  }
-  @media (max-height: 700px){
-    height: calc((100vh - 2rem) / 1 + 2px);
-  }
-
-  & > ${CardContainer} {
-    gap: 0;
-    flex-direction: column;
-    color: var(--white);
-    & > h2 {
-      font-weight: 300;
-      text-align: right;
-      letter-spacing: initial;
-    }
-  }
-`
+`;
 
 const ThumbnailProject = styled.div`
   width: 10rem;
@@ -240,38 +190,60 @@ const ThumbnailProject = styled.div`
   border-radius: 1rem;
   position: relative;
   display: flex;
-`
+`;
 
 const WrapperThumbnailProject1 = styled(ThumbnailProject)`
-  background: #CFEFBF;
-`
+  background: #cfefbf;
+`;
 
 const ThumbnailProject1 = () => {
   return (
     <WrapperThumbnailProject1>
-      <div style={{ position: "absolute", top: 0, left: "calc(50% - 1px)", height: "100%", width: "2px", background: "black" }} />
-      <p style={{ fontFamily: "JetBrains", fontSize: "var(--text-medium)", flex: 1, alignSelf: "center", textAlign: "center", background: "#CFEFBF", zIndex: 1, padding: "0.5rem 0" }}>09h00</p>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: "calc(50% - 1px)",
+          height: "100%",
+          width: "2px",
+          background: "black",
+        }}
+      />
+      <p
+        style={{
+          fontFamily: "JetBrains",
+          fontSize: "var(--text-medium)",
+          flex: 1,
+          alignSelf: "center",
+          textAlign: "center",
+          background: "#CFEFBF",
+          zIndex: 1,
+          padding: "0.5rem 0",
+        }}
+      >
+        09h00
+      </p>
     </WrapperThumbnailProject1>
-  )
-}
+  );
+};
 
 const ProjectCard = styled(Card)`
   & > ${CardContainer} {
     flex-direction: column;
   }
-`
+`;
 
 const TopCard = styled.div`
   display: flex;
   flex: 1;
   gap: 3rem;
-`
+`;
 
-const ProjectCardTextWrapper = styled.div``
+const ProjectCardTextWrapper = styled.div``;
 
 const BottomCard = styled.div`
   align-self: baseline;
-`
+`;
 
 const ProjectTypeTag = styled.div`
   font-size: var(--text-medium);
@@ -279,23 +251,127 @@ const ProjectTypeTag = styled.div`
   border: solid 1px;
   border-radius: 2rem;
   font-weight: 300;
-`
+`;
+
+const ProjectTypeSTag = styled(ProjectTypeTag)`
+  padding: 0;
+  width: 2.5rem;
+  height: 2.5rem;
+  text-align: center;
+  line-height: 2.2rem;
+  position: absolute;
+  top: 0.5rem;
+`;
 
 const ClientTag = () => {
   return (
-    <ProjectTypeTag style={{ borderColor: "var(--red)", color: "var(--red)" }}>client</ProjectTypeTag>
-  )
-}
+    <ProjectTypeTag style={{ borderColor: "var(--red)", color: "var(--red)" }}>
+      client
+    </ProjectTypeTag>
+  );
+};
 
 const PersoTag = () => {
   return (
-    <ProjectTypeTag style={{ borderColor: "var(--purple)", color: "var(--purple)" }}>perso</ProjectTypeTag>
-  )
-}
+    <ProjectTypeTag
+      style={{ borderColor: "var(--purple)", color: "var(--purple)" }}
+    >
+      perso
+    </ProjectTypeTag>
+  );
+};
+
+const ClientSTag = () => {
+  return (
+    <div>
+    <ProjectTypeSTag style={{ borderColor: "var(--red)", color: "var(--red)" }}>
+      c
+    </ProjectTypeSTag>
+    </div>
+  );
+};
+
+const PersoSTag = () => {
+  return (
+    <div>
+    <ProjectTypeSTag
+      style={{ borderColor: "var(--purple)", color: "var(--purple)" }}
+    >
+      p
+    </ProjectTypeSTag>
+    </div>
+  );
+};
 
 const LineBreak = styled.div`
   height: 0.75rem;
-`
+`;
+
+const TOCCard = styled(CardWrapper)`
+  position: absolute;
+  background: black;
+  bottom: -1px;
+  right: -1px;
+  border-radius: 2rem 2rem 0 2rem;
+
+  @media (max-width: 3900px) {
+    width: calc(100% / 6 + 2px);
+  }
+  @media (max-width: 3250px) {
+    width: calc(100% / 5 + 2px);
+  }
+  @media (max-width: 2600px) {
+    width: calc(100% / 4 + 2px);
+  }
+  @media (max-width: 1950px) {
+    width: calc(100% / 3 + 2px);
+  }
+  @media (max-width: 1300px) {
+    width: calc(100% / 2 + 2px);
+  }
+  @media (max-width: 650px) {
+    width: calc(100% / 1 + 2px);
+  }
+
+  @media (max-height: 2800px) {
+    height: calc((100vh - 2rem) / 4 + 2px);
+  }
+  @media (max-height: 2100px) {
+    height: calc((100vh - 2rem) / 3 + 2px);
+  }
+  @media (max-height: 1400px) {
+    height: calc((100vh - 2rem) / 2 + 2px);
+  }
+  @media (max-height: 700px) {
+    height: calc((100vh - 2rem) / 1 + 2px);
+  }
+
+  & > ${CardContainer} {
+    display: table;
+    border-spacing: 0.75rem 0;
+    color: var(--white);
+  }
+`;
+
+const TOCLine = styled.a`
+  display: table-row;
+  & > h2 {
+    font-weight: 300;
+    text-align: right;
+    letter-spacing: initial;
+    display: table-cell;
+  }
+  & > div {
+    display: table-cell;
+    width: 2.5rem;
+    position: relative;
+  }
+  & > h3 {
+    font-weight: 300;
+    letter-spacing: initial;
+    display: table-cell;
+  }
+`;
 
 function App() {
   return (
@@ -314,9 +390,14 @@ function App() {
       <ScrollMosaic>
         <PresentationCard>
           <CardContainer>
-            <h1><u>Baptiste Garcia</u>,</h1>
+            <h1>
+              <u>Baptiste Garcia</u>,
+            </h1>
             <LineBreak />
-            <h2>Graphic Designer &<br />Creative Developper</h2>
+            <h2>
+              Graphic Designer &<br />
+              Creative Developper
+            </h2>
             <LineBreak />
             <p>based in Paris.</p>
           </CardContainer>
@@ -326,10 +407,123 @@ function App() {
             <TopCard>
               <ThumbnailProject1 />
               <ProjectCardTextWrapper>
-                <h2><u>SemWeb.Pro</u></h2>
+                <h2>
+                  <u>SemWeb.Pro</u>
+                </h2>
                 <LineBreak />
                 <h3>website</h3>
-                <p>design +<br />front-end</p>
+                <p>
+                  design +<br />
+                  front-end
+                </p>
+                <ul>
+                  <li>Timeline</li>
+                  <li>Figma</li>
+                  <li>React</li>
+                  <li>Styled-Components</li>
+                </ul>
+              </ProjectCardTextWrapper>
+            </TopCard>
+            <BottomCard>
+              <ClientTag />
+            </BottomCard>
+          </CardContainer>
+        </ProjectCard>
+        <ProjectCard>
+          <CardContainer>
+            <TopCard>
+              <ThumbnailProject1 />
+              <ProjectCardTextWrapper>
+                <h2>
+                  <u>SemWeb.Pro</u>
+                </h2>
+                <LineBreak />
+                <h3>website</h3>
+                <p>
+                  design +<br />
+                  front-end
+                </p>
+                <ul>
+                  <li>Timeline</li>
+                  <li>Figma</li>
+                  <li>React</li>
+                  <li>Styled-Components</li>
+                </ul>
+              </ProjectCardTextWrapper>
+            </TopCard>
+            <BottomCard>
+              <ClientTag />
+            </BottomCard>
+          </CardContainer>
+        </ProjectCard>
+        <ProjectCard>
+          <CardContainer>
+            <TopCard>
+              <ThumbnailProject1 />
+              <ProjectCardTextWrapper>
+                <h2>
+                  <u>SemWeb.Pro</u>
+                </h2>
+                <LineBreak />
+                <h3>website</h3>
+                <p>
+                  design +<br />
+                  front-end
+                </p>
+                <ul>
+                  <li>Timeline</li>
+                  <li>Figma</li>
+                  <li>React</li>
+                  <li>Styled-Components</li>
+                </ul>
+              </ProjectCardTextWrapper>
+            </TopCard>
+            <BottomCard>
+              <ClientTag />
+            </BottomCard>
+          </CardContainer>
+        </ProjectCard>
+        <ProjectCard>
+          <CardContainer>
+            <TopCard>
+              <ThumbnailProject1 />
+              <ProjectCardTextWrapper>
+                <h2>
+                  <u>SemWeb.Pro</u>
+                </h2>
+                <LineBreak />
+                <h3>website</h3>
+                <p>
+                  design +<br />
+                  front-end
+                </p>
+                <ul>
+                  <li>Timeline</li>
+                  <li>Figma</li>
+                  <li>React</li>
+                  <li>Styled-Components</li>
+                </ul>
+              </ProjectCardTextWrapper>
+            </TopCard>
+            <BottomCard>
+              <ClientTag />
+            </BottomCard>
+          </CardContainer>
+        </ProjectCard>
+        <ProjectCard>
+          <CardContainer>
+            <TopCard>
+              <ThumbnailProject1 />
+              <ProjectCardTextWrapper>
+                <h2>
+                  <u>SemWeb.Pro</u>
+                </h2>
+                <LineBreak />
+                <h3>website</h3>
+                <p>
+                  design +<br />
+                  front-end
+                </p>
                 <ul>
                   <li>Timeline</li>
                   <li>Figma</li>
@@ -347,39 +541,92 @@ function App() {
         <CardWrapper />
         <CardWrapper />
         <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
-        <CardWrapper />
         <FixedMosaic>
           <TOCCard>
             <CardContainer>
-              <h2><u>Logilab.fr</u></h2>
-              <h2><u>SemWeb.Pro</u></h2>
-              <h2><u>Logilab</u></h2>
-              <h2><u>CubicWeb.org</u></h2>
-              <h2><u>FranceArchives</u></h2>
-              <h2><u>Data-BnF</u></h2>
-              <h2><u>Napoleonica</u></h2>
-              <h2><u>LeBowVosgien</u></h2>
-              <h2><u>StretchTypo</u></h2>
-              <h2><u>WaterPoster</u></h2>
-              <h2><u>LePetitSalon</u></h2>
+              <TOCLine>
+                <h2>
+                  <u>Logilab.fr</u>
+                </h2>
+                <ClientSTag />
+                <h3>website</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>SemWeb.Pro</u>
+                </h2>
+                <PersoSTag />
+                <h3>website</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>Logilab</u>
+                </h2>
+                <ClientSTag />
+                <h3>calendar</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>CubicWeb.org</u>
+                </h2>
+                <ClientSTag />
+                <h3>website</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>FranceArchives</u>
+                </h2>
+                <PersoSTag />
+                <h3>website</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>Data-BnF</u>
+                </h2>
+                <PersoSTag />
+                <h3>website</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>Napoleonica</u>
+                </h2>
+                <ClientSTag />
+                <h3>website</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>LeBowVosgien</u>
+                </h2>
+                <PersoSTag />
+                <h3>website</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>StretchTypo</u>
+                </h2>
+                <ClientSTag />
+                <h3>typo</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>WaterPoster</u>
+                </h2>
+                <PersoSTag />
+                <h3>video</h3>
+              </TOCLine>
+              <TOCLine>
+                <h2>
+                  <u>LePetitSalon</u>
+                </h2>
+                <PersoSTag />
+                <h3>catalog</h3>
+              </TOCLine>
             </CardContainer>
           </TOCCard>
         </FixedMosaic>
       </ScrollMosaic>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
