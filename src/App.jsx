5a7eef9b2e809ad2a1e0214014f5a25f28a0d88f
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-08-22 17:27:58 +0200

    fix: make cards appear sooner

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ function App() {
     tl.set(cardsRef.current, { opacity: 0, scale: 0.9, y: "6rem" });
 
     ScrollTrigger.batch(cardsRef.current, {
-      start: "top 97%",
+      start: "top 98%",
       onEnter: (batch) => {
         tl.to(batch, {
           opacity: 1,
