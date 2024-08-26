9bef583f451ff2aabb09ca4d77517ed7ebea7ab2
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-08-26 15:41:36 +0200

    fix: set card height for smoother fullscreen animation

--- src/App.jsx
+++ src/App.jsx
@@ -136,12 +136,10 @@ function App() {
       duration: 0,
     });
 
-    if (window.innerWidth < 650 || index === 0) {
-      timeline.to(wrapper, {
-        height: wrapperHeight,
-        duration: 0,
-      });
-    }
+    timeline.to(wrapper, {
+      height: wrapperHeight,
+      duration: 0,
+    });
 
     timeline
       .to(
