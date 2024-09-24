4a33551d669243a005160548ffe08feea1d8da0c
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-09-24 22:02:58 +0200

    feat: update thumbnails

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ import { ProjectCard, PresentationCard, TOCCard } from "./components/Cards";
 
 import { ProjectPage as DefaultProjectPage } from "./components/projects/DefaultProject";
 import { ProjectPage as PageBowVosgien } from "./components/projects/PP_bowvosgien";
-import ThumbLeBowVosgien from "./assets/projects/le_bow_vosgien/mockup_le_bow_vosgien4.jpg";
+import ThumbLeBowVosgien from "./assets/projects/le_bow_vosgien/mockup_le_bow_vosgien5.jpg";
+import ThumbLogilabFr from "./assets/projects/logilab_fr/mockup_logilab_fr.jpg";
 import ThumbDefault1 from "./assets/mockup_macbook.png";
 import ThumbDefault2 from "./assets/mockup_iphone.png";
 import ThumbDefault3 from "./assets/mockup_imac.png";
@@ -187,7 +188,7 @@ function App() {
       subtitle: "calendar",
       description: "design + front-end",
       keywords: "Timeline — Figma — React",
-      thumbnail: ThumbDefault3,
+      thumbnail: ThumbLogilabFr,
       projectpage: DefaultProjectPage,
     },
     {
