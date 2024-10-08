f7a18e8be4d9991116991606c91f8fa8dda96af9
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-10-08 17:57:51 +0200

    feat: add visit button remove useless code last project page

--- src/components/projects/PP_cubicweborg.jsx
+++ src/components/projects/PP_cubicweborg.jsx
@@ -1,16 +1,12 @@
 import React, { useRef } from "react";
-import styled from "styled-components";
 import {
   ProjectPageWrapper,
   ProjectPageContent,
   FullScreenImage,
-  FullScreenBackground,
   TextMedium,
   TextSmall,
-  DoubleColumns,
   MockUpDouble,
   VisitSiteButton,
-  Empty,
 } from "../../assets/PageAssets";
 
 import Image0 from "../../assets/projects/cubicweb/cubicweb_screen_1440x900.png";
@@ -42,14 +38,18 @@ export const ProjectPage = () => {
           homepage, using small, segmented sections to make the project
           appealing despite its technical complexity.
         </TextSmall>
-        <FullScreenImage scroller={containerRef} src={Image2} $isSquareOnMobile={true} />
+        <FullScreenImage
+          scroller={containerRef}
+          src={Image2}
+          $isSquareOnMobile={true}
+        />
         <TextMedium scroller={containerRef}>
           The project also involves redesigning the technical blog associated
           with the framework, reusing elements from the main site —&nbsp;such as
           the color palette, typography, and layout template&nbsp;— to maintain
           coherence between the two sites.
         </TextMedium>
-        <Empty />
+        <VisitSiteButton href="https://www.cubicweb.org/" />
       </ProjectPageContent>
     </ProjectPageWrapper>
   );
